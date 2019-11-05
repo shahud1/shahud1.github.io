cac768aca8e0e2aa27262f012e0f940d287caccb
Enki <enki@machome.local>
2019-11-05 19:00:38 +0700

    Initial commit

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-echo hello
-
-mongoexport --uri="mongodb://tusmartcity:tu2018@log.haupcar.com/tusmartcity" --collection=interval --type=csv --out=/Users/enki/Desktop/shahud1.github.io/raw.csv --fields _id,vehicleid,reservationno,logtime,latitude,longitude,speed,heading,hdop,fuel,charge,km,doorlockstate,enginestate
-
-echo files saved to raw.csv
