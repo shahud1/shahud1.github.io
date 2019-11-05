4005d34bc1e878af0cfb39c7f01de186cc88ab1c
Enki <enki@machome.local>
2019-11-05 17:41:24 +0700

    Initial commit

--- masterupdate.sh
+++ masterupdate.sh
@@ -14,6 +14,7 @@ cp /Users/enki/Desktop/shahud1.github.io/car1latest.geojson /Users/enki/Desktop/
 cp /Users/enki/Desktop/shahud1.github.io/car2latest.geojson /Users/enki/Desktop/temp/car2latest.geojson
 
 cd Desktop/temp
+git pull
 git add --all
 git commit -m "Initial commit"
 git push -u origin master
